a9b817a6f3e75427e0f38b2d70b787273a5f9301
Nícolas Ribeiro <53842783+nicolasrls@users.noreply.github.com>
2019-11-03 00:17:59 -0300

    Questão 4 base

---
+++ Lista de TecProg 4(MAP)/map4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <map>
+#define ALTA = "alta"
+#define MEDIA = "media"
+#define BAIXA = "baixa"
+
+using namespace std;
+
+int main()
+{
+
+    string resp;
+    string local;
+
+    map<string, string, string> localidade;
+
+
+    return 0;
+}
