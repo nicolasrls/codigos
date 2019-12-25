11a4e32bc7af6ae905ae1d6181afe3565b17c464
nicolasrls <nicolaufacul@gmail.com>
2019-12-25 17:52:05 -0300

    Questão 5 incompleta

--- Python3/Exercícios Lista/Lista/q5.py
+++ Python3/Exercícios Lista/Lista/q5.py
@@ -6,8 +6,12 @@ while numero != freio:
     l.append(numero)
     numero = int(input("Número: "))
 
+l.sort()
+
 for x in l:
-    while l.count(x) != 1:
-        print("O Número ", x, "aparece", l.count(x), "vezes.")
-    else:
-        print("O Número", x, "aparece", l.count(x), "vez")
+    if l.count(x) == 1:
+        print("O Número ", x, "aparece", l.count(x), "vez.")
+    else:        
+        print("O Número ", x, "aparece", l.count(x), "vezes.")        
+        
+    
