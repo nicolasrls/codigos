da6ac3756927091720f479c9a7439cd272c969ba
Nícolas Ribeiro <nicolas.ribeiro@academico.ifpb.edu.br>
2020-02-11 15:29:23 -0300

    Adicionando Aula de Cálculo numérico

---
+++ Matlab/Programa 1/eq2grau.m
@@ -0,0 +1,12 @@
+clear all
+clc
+close all
+a = input('a = ');
+b = input('b = ');
+c = input('c = ');
+delta = b^2 - 4*a*c;
+x1 = (-b+sqrt(delta))/(2*a);
+x2 = (-b-sqrt(delta))/(2*a);
+x = [ x1,x2];
+disp('A solução vale :');
+disp(x);
